539cf39b7c756c27922c6a4d3862c6e4aba6e50b
T R A $ H <sulthons.a19@gmail.com>
2023-10-05 15:39:45 +0700

    Tugas 13 JavaScript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas13.js
@@ -0,0 +1,10 @@
+function tugas13() {
+  var tugas = ["Pisang", "Jeruk"];
+  console.log(tugas);
+  tugas.unshift("Apel", "Mangga", "Pisang", "Jeruk");
+
+  return tugas;
+}
+
+console.log(tugas13());
+// Unshift //
